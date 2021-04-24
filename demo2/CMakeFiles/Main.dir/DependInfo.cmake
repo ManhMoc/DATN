337804613153497656df9fef50b29b4135dffe3e
namlb <namlb1606@gmail.com>
2021-04-24 17:29:18 +0100

    [DATN] test

---
+++ demo2/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/opencv/demo2/DetectChars.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/DetectChars.cpp.o"
+  "/home/pi/opencv/demo2/DetectPlates.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/DetectPlates.cpp.o"
+  "/home/pi/opencv/demo2/Main.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/Main.cpp.o"
+  "/home/pi/opencv/demo2/PossibleChar.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/PossibleChar.cpp.o"
+  "/home/pi/opencv/demo2/PossiblePlate.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/PossiblePlate.cpp.o"
+  "/home/pi/opencv/demo2/Preprocess.cpp" "/home/pi/opencv/demo2/CMakeFiles/Main.dir/Preprocess.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
